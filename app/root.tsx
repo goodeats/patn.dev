884ccd6686e93c1142fd75800c3c8ecfc3e0e172
Pat Needham <patrick.h.needham@gmail.com>
2024-02-01 03:51:52 -0500

    removed commented out code

--- app/root.tsx
+++ app/root.tsx
@@ -268,67 +268,6 @@ function AppWithProviders() {
 
 export default withSentry(AppWithProviders)
 
-// function UserDropdown() {
-// 	const user = useUser()
-// 	const submit = useSubmit()
-// 	const formRef = useRef<HTMLFormElement>(null)
-// 	return (
-// 		<DropdownMenu>
-// 			<DropdownMenuTrigger asChild>
-// 				<Button asChild variant="secondary">
-// 					<Link
-// 						to={`/users/${user.username}`}
-// 						// this is for progressive enhancement
-// 						onClick={e => e.preventDefault()}
-// 						className="flex items-center gap-2"
-// 					>
-// 						<img
-// 							className="h-8 w-8 rounded-full object-cover"
-// 							alt={user.name ?? user.username}
-// 							src={getUserImgSrc(user.image?.id)}
-// 						/>
-// 						<span className="text-body-sm font-bold">
-// 							{user.name ?? user.username}
-// 						</span>
-// 					</Link>
-// 				</Button>
-// 			</DropdownMenuTrigger>
-// 			<DropdownMenuPortal>
-// 				<DropdownMenuContent sideOffset={8} align="start">
-// 					<DropdownMenuItem asChild>
-// 						<Link prefetch="intent" to={`/users/${user.username}`}>
-// 							<Icon className="text-body-md" name="avatar">
-// 								Profile
-// 							</Icon>
-// 						</Link>
-// 					</DropdownMenuItem>
-// 					<DropdownMenuItem asChild>
-// 						<Link prefetch="intent" to={`/users/${user.username}/notes`}>
-// 							<Icon className="text-body-md" name="pencil-2">
-// 								Notes
-// 							</Icon>
-// 						</Link>
-// 					</DropdownMenuItem>
-// 					<DropdownMenuItem
-// 						asChild
-// 						// this prevents the menu from closing before the form submission is completed
-// 						onSelect={event => {
-// 							event.preventDefault()
-// 							submit(formRef.current)
-// 						}}
-// 					>
-// 						<Form action="/logout" method="POST" ref={formRef}>
-// 							<Icon className="text-body-md" name="exit">
-// 								<button type="submit">Logout</button>
-// 							</Icon>
-// 						</Form>
-// 					</DropdownMenuItem>
-// 				</DropdownMenuContent>
-// 			</DropdownMenuPortal>
-// 		</DropdownMenu>
-// 	)
-// }
-
 /**
  * @returns the user's theme preference, or the client hint theme if the user
  * has not set a preference.
