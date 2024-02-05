43d43d57333f78bb2b1ed3b3debd93656e856b26
Pat Needham <patrick.h.needham@gmail.com>
2024-02-05 18:55:12 -0500

    moved header and logo to component

---
+++ app/components/layout/header.tsx
@@ -0,0 +1,19 @@
+import { useOptionalUser } from '#app/utils/user'
+import { Logo, UserDropdown } from '../templates'
+
+const MainHeader = () => {
+	const user = useOptionalUser()
+	return (
+		<header className="container py-6">
+			<nav className="flex flex-wrap items-center justify-between gap-4 sm:flex-nowrap md:gap-8">
+				<Logo />
+
+				<div className="flex items-center gap-10">
+					{user && <UserDropdown />}
+				</div>
+			</nav>
+		</header>
+	)
+}
+
+export { MainHeader }
--- app/root.tsx
+++ app/root.tsx
@@ -11,7 +11,6 @@ import {
 	type MetaFunction,
 } from '@remix-run/node'
 import {
-	Link,
 	Links,
 	LiveReload,
 	Meta,
@@ -25,8 +24,8 @@ import {
 import { withSentry } from '@sentry/remix'
 import { HoneypotProvider } from 'remix-utils/honeypot/react'
 import { z } from 'zod'
-import { GeneralErrorBoundary } from './components/layout'
-import { EpicProgress, useToast, UserDropdown } from './components/templates'
+import { GeneralErrorBoundary, MainHeader } from './components/layout'
+import { EpicProgress, useToast, Logo } from './components/templates'
 import { Icon, href as iconsHref, EpicToaster } from './components/ui'
 import tailwindStyleSheetUrl from './styles/tailwind.css'
 import { getUserId, logout } from './utils/auth.server.ts'
@@ -40,7 +39,6 @@ import { useRequestInfo } from './utils/request-info.ts'
 import { type Theme, setTheme, getTheme } from './utils/theme.server.ts'
 import { makeTimings, time } from './utils/timing.server.ts'
 import { getToast } from './utils/toast.server.ts'
-import { useOptionalUser } from './utils/user.ts'
 
 export const links: LinksFunction = () => {
 	return [
@@ -208,29 +206,15 @@ function Document({
 function App() {
 	const data = useLoaderData<typeof loader>()
 	const nonce = useNonce()
-	const user = useOptionalUser()
 	const theme = useTheme()
-	// const matches = useMatches()
-	// const isOnSearchPage = matches.find(m => m.id === 'routes/users+/index')
-	// const searchBar = isOnSearchPage ? null : <SearchBar status="idle" />
-	const searchBar = null
 	useToast(data.toast)
+
+	console.log(data.ENV)
 
 	return (
 		<Document nonce={nonce} theme={theme} env={data.ENV}>
 			<div className="flex h-screen flex-col justify-between">
-				<header className="container py-6">
-					<nav className="flex flex-wrap items-center justify-between gap-4 sm:flex-nowrap md:gap-8">
-						<Logo />
-						<div className="ml-auto hidden max-w-sm flex-1 sm:block">
-							{searchBar}
-						</div>
-						<div className="flex items-center gap-10">
-							{user && <UserDropdown />}
-						</div>
-						<div className="block w-full sm:hidden">{searchBar}</div>
-					</nav>
-				</header>
+				<MainHeader />
 
 				<div className="flex-1">
 					<Outlet />
@@ -244,19 +228,6 @@ function App() {
 			<EpicToaster closeButton position="top-center" theme={theme} />
 			<EpicProgress />
 		</Document>
-	)
-}
-
-function Logo() {
-	return (
-		<Link to="/" className="group grid leading-snug">
-			<span className="font-light transition group-hover:-translate-x-1">
-				patn
-			</span>
-			<span className="font-bold transition group-hover:translate-x-1">
-				dev
-			</span>
-		</Link>
 	)
 }
 
