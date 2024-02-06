49b39a771ee1a4da83c0ebf1c2d86a1af01b4549
Pat Needham <patrick.h.needham@gmail.com>
2024-02-05 23:31:03 -0500

    refactor

--- app/routes/_marketing+/index.tsx
+++ app/routes/_marketing+/index.tsx
@@ -47,35 +47,48 @@ export default function Index() {
 			/>
 		)
 	}
+
+	const Heading = () => {
+		return (
+			<h1
+				data-heading
+				className="mt-8 animate-slide-top text-4xl font-medium text-foreground [animation-delay:0.3s] [animation-fill-mode:backwards] md:text-5xl xl:mt-4 xl:animate-slide-left xl:text-6xl xl:[animation-delay:0.8s] xl:[animation-fill-mode:backwards]"
+			>
+				<a href="https://github.com/goodeats/patn.dev">Pat Needham</a>
+			</h1>
+		)
+	}
+
+	const CallToAction = () => {
+		return (
+			<p
+				data-paragraph
+				className="mt-6 animate-slide-top text-xl/7 text-muted-foreground [animation-fill-mode:backwards] [animation-delay:0.8s] xl:mt-8 xl:animate-slide-left xl:text-xl/6 xl:leading-10 xl:[animation-fill-mode:backwards] xl:[animation-delay:1s]"
+			>
+				Check out the{' '}
+				<a
+					className="underline hover:no-underline"
+					href="https://github.com/goodeats/patn.dev"
+				>
+					GitHub Repo
+				</a>{' '}
+				for my portfolio site while it is under construction! 🚧
+			</p>
+		)
+	}
+
 	return (
 		<main className="font-poppins grid h-full place-items-center">
 			<div className="grid place-items-center px-4 py-16 xl:grid-cols-2 xl:gap-24">
 				<div className="flex max-w-md flex-col items-center text-center xl:order-2 xl:items-start xl:text-left">
 					<a
-						href="https://www.epicweb.dev/stack"
-						className="animate-slide-top [animation-fill-mode:backwards] xl:animate-slide-left xl:[animation-delay:0.5s] xl:[animation-fill-mode:backwards]"
+						href="https://github.com/goodeats/patn.dev"
+						className="animate-slide-top [animation-fill-mode:backwards] xl:animate-slide-left xl:[animation-fill-mode:backwards] xl:[animation-delay:0.5s]"
 					>
 						<Logo />
 					</a>
-					<h1
-						data-heading
-						className="mt-8 animate-slide-top text-4xl font-medium text-foreground [animation-fill-mode:backwards] [animation-delay:0.3s] md:text-5xl xl:mt-4 xl:animate-slide-left xl:text-6xl xl:[animation-fill-mode:backwards] xl:[animation-delay:0.8s]"
-					>
-						<a href="https://github.com/goodeats/patn.dev">Pat Needham</a>
-					</h1>
-					<p
-						data-paragraph
-						className="mt-6 animate-slide-top text-xl/7 text-muted-foreground [animation-fill-mode:backwards] [animation-delay:0.8s] xl:mt-8 xl:animate-slide-left xl:text-xl/6 xl:leading-10 xl:[animation-fill-mode:backwards] xl:[animation-delay:1s]"
-					>
-						Check out the{' '}
-						<a
-							className="underline hover:no-underline"
-							href="https://github.com/goodeats/patn.dev"
-						>
-							GitHub Repo
-						</a>{' '}
-						for my portfolio site while it is under construction!
-					</p>
+					<Heading />
+					<CallToAction />
 				</div>
 				<LogoGrid logos={logos} />
 			</div>
