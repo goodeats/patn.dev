979332d1f6077e1b1883834b0b2ed3b906b3dea4
Pat Needham <patrick.h.needham@gmail.com>
2024-02-05 19:35:57 -0500

    env conditional header options

--- app/components/layout/page-header.tsx
+++ app/components/layout/page-header.tsx
@@ -1,15 +1,37 @@
+import { Link } from '@remix-run/react'
+import { useRootLoaderData } from '#app/root'
 import { useOptionalUser } from '#app/utils/user'
-import { Logo, UserDropdown } from '../templates'
+import { Logo, ThemeSwitch, UserDropdown } from '../templates'
+import { Button } from '../ui'
 
 const PageHeader = () => {
+	const { ENV } = useRootLoaderData()
 	const user = useOptionalUser()
+
+	const Theme = () => {
+		if (ENV.MODE === 'development') return null
+
+		return <ThemeSwitch />
+	}
+
+	const LoginButton = () => {
+		if (ENV.MODE === 'production') return null
+
+		return (
+			<Button asChild variant="default" size="lg">
+				<Link to="/login">Log In</Link>
+			</Button>
+		)
+	}
+
 	return (
 		<header className="container py-6">
 			<nav className="flex flex-wrap items-center justify-between gap-4 sm:flex-nowrap md:gap-8">
 				<Logo />
 
 				<div className="flex items-center gap-10">
-					{user && <UserDropdown />}
+					<Theme />
+					{user ? <UserDropdown /> : <LoginButton />}
 				</div>
 			</nav>
 		</header>
