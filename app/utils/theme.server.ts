1704c46cbc1d3c17debf927e1b613273122b7287
Pat Needham <patrick.h.needham@gmail.com>
2024-02-09 23:11:26 -0500

    logs

--- app/utils/theme.server.ts
+++ app/utils/theme.server.ts
@@ -6,11 +6,13 @@ export type Theme = 'light' | 'dark'
 export function getTheme(request: Request): Theme | null {
 	const cookieHeader = request.headers.get('cookie')
 	const parsed = cookieHeader ? cookie.parse(cookieHeader)[cookieName] : 'light'
+	console.log('getTheme: ', parsed)
 	if (parsed === 'light' || parsed === 'dark') return parsed
 	return null
 }
 
 export function setTheme(theme: Theme | 'system') {
+	console.log('setTheme: ', theme)
 	if (theme === 'system') {
 		return cookie.serialize(cookieName, '', { path: '/', maxAge: -1 })
 	} else {
