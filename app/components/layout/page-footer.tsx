bd6cb53ec2778361f484f4306bc92a8410f96df7
Pat Needham <patrick.h.needham@gmail.com>
2024-02-10 01:15:20 -0500

    reduce component loading logic

--- app/components/layout/page-footer.tsx
+++ app/components/layout/page-footer.tsx
@@ -1,29 +1,30 @@
-import { useRootLoaderData } from '#app/root'
 import { Logo, ThemeSwitch } from '../templates'
 import { Icon } from '../ui'
 
 const PageFooter = () => {
-	const { ENV } = useRootLoaderData()
-	const isProduction = ENV.MODE === 'production'
-
 	return (
 		<div className="container flex justify-between pb-5">
 			<Logo />
-			{isProduction ? (
-				<a
-					href="https://github.com/goodeats"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<Icon name="github-logo" size="lg">
-						<span className="sr-only">Github</span>
-					</Icon>
-				</a>
-			) : (
-				<ThemeSwitch />
-			)}
+			<a
+				href="https://github.com/goodeats"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				<Icon name="github-logo" size="lg">
+					<span className="sr-only">Github</span>
+				</Icon>
+			</a>
 		</div>
 	)
 }
 
-export { PageFooter }
+const PageFooterDev = () => {
+	return (
+		<div className="container flex justify-between pb-5">
+			<Logo />
+			<ThemeSwitch />
+		</div>
+	)
+}
+
+export { PageFooter, PageFooterDev }
