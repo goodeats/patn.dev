fafe564dbd92e142fbf2331d7c5f098df3a060bc
Pat Needham <patrick.h.needham@gmail.com>
2024-02-12 21:03:30 -0500

    seed posts

--- tests/e2e/admin/pages/pages-utils.ts
+++ tests/e2e/admin/pages/pages-utils.ts
@@ -41,12 +41,32 @@ export const createPages = [
 		description: 'Landing page for the website.',
 		slug: 'home',
 		published: true,
+		posts: {
+			create: [
+				{
+					title: 'Welcome to my website!',
+					slug: 'welcome-to-my-website',
+					description: 'This is the first post on my website.',
+					content: 'This is the first post on my website.',
+				},
+			],
+		},
 	},
 	{
 		name: 'Projects',
 		description: 'A list of projects I have worked on.',
 		slug: 'projects',
 		published: true,
+		posts: {
+			create: [
+				{
+					title: 'Project XYZ',
+					slug: 'project-xyz',
+					description: 'This is the first project.',
+					content: 'This is the first project.',
+				},
+			],
+		},
 	},
 	{
 		name: 'Blog',
@@ -59,11 +79,35 @@ export const createPages = [
 		description: 'A little bit about me.',
 		slug: 'about',
 		published: true,
+		posts: {
+			create: [
+				{
+					title: 'About Me',
+					slug: 'about-me',
+					description: 'This is the first post about me.',
+					content: 'This is the first post about me.',
+					published: true,
+					publishedAt: new Date(),
+				},
+			],
+		},
 	},
 	{
 		name: 'Contact',
 		description: 'Get in touch with me.',
 		slug: 'contact',
 		published: true,
+		posts: {
+			create: [
+				{
+					title: 'Contact Me',
+					slug: 'contact-me',
+					description: 'This is the first post about contacting me.',
+					content: 'This is the first post about contacting me.',
+					published: true,
+					publishedAt: new Date(),
+				},
+			],
+		},
 	},
 ]
